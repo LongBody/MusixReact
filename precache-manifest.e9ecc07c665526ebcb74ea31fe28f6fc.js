97e8fbb1ec16e6fe7af5906a5df7caf94ee07380
LongBody <54003924+LongBody@users.noreply.github.com>
2020-08-16 21:03:06 +0700

    Updates

---
+++ precache-manifest.e9ecc07c665526ebcb74ea31fe28f6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482d23d1095b72b0da3b47e4ddd8e12d",
+    "url": "/MusixReact/index.html"
+  },
+  {
+    "revision": "ed32fc07ef6a7c45890e",
+    "url": "/MusixReact/static/css/main.848a5677.chunk.css"
+  },
+  {
+    "revision": "ab0f0dd2a5e9134bcf9d",
+    "url": "/MusixReact/static/js/2.aa7a639b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MusixReact/static/js/2.aa7a639b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed32fc07ef6a7c45890e",
+    "url": "/MusixReact/static/js/main.f2d4b264.chunk.js"
+  },
+  {
+    "revision": "f447705cf208d8043d06",
+    "url": "/MusixReact/static/js/runtime-main.d1874dda.js"
+  }
+]);
